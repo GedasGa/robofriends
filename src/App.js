ecf5043851be317a823fd9cdd24ab61f9988e91e
gedasga <gedas.gardauskas@gmail.com>
2018-10-10 23:14:35 +0900

    Fetch robots data from typicode.com

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
     };
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
 
     onSearchChange = (event) => {
         this.setState({ searchField: event.target.value });
@@ -21,14 +26,19 @@ class App extends Component {
         const { robots, searchField } = this.state;
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-        return (
-            <div className='tc'>
-                <h1 className='f2'>Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        );
+        });
+        if (this.state.robots.length === 0) {
+            return <h1>Loading</h1>
+        }
+        else {
+            return (
+                <div className='tc'>
+                    <h1 className='f2'>Robofriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     };
 }
 
