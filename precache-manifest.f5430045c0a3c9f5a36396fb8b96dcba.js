4681678696b0f017a7e991cec114100d7bd4b233
gedasga <gedas.gardauskas@gmail.com>
2018-10-20 11:34:39 +0900

    Updates

---
+++ precache-manifest.f5430045c0a3c9f5a36396fb8b96dcba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "6a8e39926d13a9754f6f",
+    "url": "/robofriends/static/js/main.6a8e3992.chunk.js"
+  },
+  {
+    "revision": "b40a754fdb510326569f",
+    "url": "/robofriends/static/js/1.b40a754f.chunk.js"
+  },
+  {
+    "revision": "6a8e39926d13a9754f6f",
+    "url": "/robofriends/static/css/main.d5940a19.chunk.css"
+  },
+  {
+    "revision": "b40a754fdb510326569f",
+    "url": "/robofriends/static/css/1.4681b6e4.chunk.css"
+  },
+  {
+    "revision": "583c3ba07ecbfea1976bc7d6a8e18b16",
+    "url": "/robofriends/index.html"
+  }
+];
